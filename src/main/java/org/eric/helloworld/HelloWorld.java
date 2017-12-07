79feb917b235ddeba7d2de051200f04b6b1d68c0
Eric Francois <eric.francois@soprasteria.com>
2017-12-07 15:11:58 +0100

    Compile and run

--- src/main/java/org/eric/helloworld/HelloWorld.java
+++ src/main/java/org/eric/helloworld/HelloWorld.java
@@ -2,8 +2,7 @@ package org.eric.helloworld;
 
 class HelloWorld {
 
-	public static int main(String args[]) {
+	public static void main(String args[]) {
 		System.out.println("Hello world");
-		return 0;
 	}
 }
